8c3c9fe756196e4b007a478e6cd89851dde174dd
maxime.places <maxime.places@ebp.com>
2022-11-13 23:36:13 +0100

    Initial commit

---
+++ My.WASM.Dashboard/JsBundle/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require("path");
+
+module.exports = {
+    entry: ['./index.js', './src/test.js'],
+    output: {
+        filename: 'mybundle.js',
+        path: path.resolve(__dirname, '../wwwroot/js')
+    },
+};
